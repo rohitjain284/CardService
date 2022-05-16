8b819a9cb2ea0de1540b6de73e9e55f3cb19d0b1
rohitjain284 <105641635+rohitjain284@users.noreply.github.com>
2022-05-16 18:35:28 +0530

    Add files via upload

---
+++ CardService/Business/CardDetails.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using CardService.Contract;
+using ClientLibrary;
+using ClientLibrary.Contract;
+using ClientLibrary.Model;
+
+namespace CardService
+{
+    public class CardDetails : ICardDetails
+    {
+        /// <summary>
+        /// Method to get Card Details
+        /// </summary>
+        /// <param name="instituionId"></param>
+        /// <param name="cardIdentifier"></param>
+        /// <returns></returns>
+        public CardInformation GetCard(string instituionId, string cardIdentifier)
+        {
+            try
+            {
+                GetClientData oGetClient = new GetClientData();
+                IClient oClient = oGetClient.GetClientInstance(instituionId);
+                if (oClient == null)
+                {
+                    throw new Exception("Invalid Institution Id!");
+                }
+
+                return oClient.GetCard(cardIdentifier);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
